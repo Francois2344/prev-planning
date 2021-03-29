50b9f9c033bb0811ec0aa882b438e0b2c28fab7e
Francois2344 <deloufr@gmail.com>
2021-03-29 09:30:03 +0200

    remove a duplicate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Admin from "./admin/Admin";
+import Head from "./admin/Head";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Head />
+      <Admin />
+    </div>
+  );
+};
+
+export default App;
