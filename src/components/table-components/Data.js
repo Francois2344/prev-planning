8a359ee447f87a169e5253638907c9476ffd7b73
Francois2344 <deloufr@gmail.com>
2021-04-08 10:59:50 +0200

    css responsive admin-panel

--- src/components/table-components/Data.js
+++ src/components/table-components/Data.js
@@ -1,8 +1,10 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable import/prefer-default-export */
 export const DATA = [
-    {
-        col1: <input type='text' />,
-    },
-    {
-        col2:'test'
-    }
-]
+  {
+    col1: <input type="text" />,
+  },
+  {
+    col2: 'test',
+  },
+];
