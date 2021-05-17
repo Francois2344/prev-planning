234157c8c66eeb6ce8fc4cf531df867edb32d7ad
Francois2344 <deloufr@gmail.com>
2021-05-17 16:49:07 +0200

    alert input, css, react-toastify

--- src/auth/LogOutBtn.jsx
+++ src/auth/LogOutBtn.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useContext } from 'react';
 import { FcImport } from 'react-icons/fc';
 import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import AuthContext from '../service/AuthContext';
 
 const LogOutBtn = () => {
@@ -12,6 +13,7 @@ const LogOutBtn = () => {
     await axios.get('http://localhost:8000/register/logout');
     await getLoggedIn();
     history.push('/');
+    toast.info('Deconnexion');
   };
   return (
     <FcImport type="submit" onClick={logOut} size={40}>
